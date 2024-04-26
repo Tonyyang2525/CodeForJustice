942cdaaef9970bc567dee2038ff2aef24ce74857
BryandMarti <164084388+BryandMarti@users.noreply.github.com>
2024-04-26 16:40:55 -0400

    Update

---
+++ plasticBryan/aboutPage/script.js
@@ -0,0 +1,17 @@
+
+// this function is for the toggle of the hamburger menu
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open")
+  icon.classList.toggle("open")
+}
+
+
+
+
+
+
+
+
+// My game should have the count scoring of the same form like pickleball, I also wanna say that I will like my code to have 4 people. 
